7431ad77f0a0d3c97227598ff8ba7d172d250bbb
Cassio Augusto <cassio_0101@hotmail.com>
2023-03-27 06:42:06 -0300

    Implementa GET /transactions

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from fastapi.responses import HTMLResponse
+from typing import List
 from sqlalchemy.orm import Session
 from . import crud, models, schemas, parsers, database
 
@@ -16,3 +17,8 @@ async def upload_cnab(file: UploadFile = File(...), db: Session = Depends(databa
         crud.create_transaction(db, schemas.TransactionCreate(**transaction))
 
     return {"message": "Arquivo CNAB processado com sucesso"}
+
+@app.get("/transactions/", response_model=List[schemas.Transaction])
+def read_transactions(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+    transactions = crud.get_transactions(db, skip=skip, limit=limit)
+    return transactions
