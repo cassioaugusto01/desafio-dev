9427763fee4efb78cf523662da5b94ba1c2fed76
Cassio Augusto <cassio_0101@hotmail.com>
2023-03-27 08:29:23 -0300

    Configurado o Postgre no Docker

--- app/database.py
+++ app/database.py
@@ -2,9 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://user:password@localhost/dbname"
+DATABASE_URL = "postgresql://usuario:senha@db:5432/desafio"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
