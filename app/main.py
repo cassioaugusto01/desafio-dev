cb5f2266d5f997f454729efe7965c017e10dc71c
Cassio Augusto <cassio_0101@hotmail.com>
2023-03-27 06:39:11 -0300

    Implementa rota /upload

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
+from fastapi.responses import HTMLResponse
+from sqlalchemy.orm import Session
+from . import crud, models, schemas, parsers, database
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+models.Base.metadata.create_all(bind=database.engine)
+
+@app.post("/upload/")
+async def upload_cnab(file: UploadFile = File(...), db: Session = Depends(database.get_db)):
+    file_content = await file.read()
+    transactions = parsers.parse_cnab(file_content.decode("utf-8"))
+
+    for transaction in transactions:
+        crud.create_transaction(db, schemas.TransactionCreate(**transaction))
+
+    return {"message": "Arquivo CNAB processado com sucesso"}
